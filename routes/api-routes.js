a1fdcfc0668b9414e37d36b783181fddc8f593f9
Brzeczyszczykiewicz <Samproschansky@gmail.com>
2019-03-02 12:30:11 -0500

    Checked file structure

---
+++ routes/api-routes.js
@@ -0,0 +1,34 @@
+// *********************************************************************************
+// api-routes.js - this file offers a set of routes for displaying and saving data to the db
+// *********************************************************************************
+
+// Dependencies
+// =============================================================
+
+// Requiring our models
+var db = require("../models");
+
+// Routes
+// =============================================================
+module.exports = function(app) {
+
+  // GET route 
+  app.get("", function(req, res) {
+    
+  });
+
+  // POST route 
+  app.post("", function(req, res) {
+    
+  });
+
+  // DELETE route 
+  app.delete("/api/todos/:id", function(req, res) {
+  
+  });
+
+  // PUT route 
+  app.put("", function(req, res) {
+
+  });
+};
