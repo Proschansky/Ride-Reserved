1a1270b622c8c16a6cd5c794a5eb5a25cba027f4
KHAUNA <jtwinturbo@gmail.com>
2019-03-07 21:15:10 -0500

    pull

--- models/riders.js
+++ models/riders.js
@@ -1,40 +1,9 @@
 module.exports = function(sequelize, DataTypes) {
     var riders = sequelize.define("Riders", {
-<<<<<<< HEAD
       name: DataTypes.STRING,
       requireNDA: DataTypes.BOOLEAN,
       preferredLanguage: DataTypes.STRING
 
-=======
-      name: {
-        type: DataTypes.STRING,
-        // AllowNull is a flag that restricts a todo from being entered if it doesn't
-        // have a text value
-        allowNull: false,
-        // len is a validation that checks that our todo is between 1 and 140 characters
-        validate: {
-          len: [1, 50]
-        }
-      },
-      interests: {
-        type: DataTypes.TEXT,
-        validate: {
-          len: [1,1000]
-        }
-      },
-      aboutMe: {
-        type: DataTypes.TEXT,
-        validate: {
-          len: [1,500]
-        }
-      },
-      languages:{
-        type: DataTypes.STRING,
-        validate: {
-          len: [1,1000]
-        }
-      },
->>>>>>> 27fe72a549811004847f3b7f7bbebbf1b351bc94
     });
     return riders;
   };
