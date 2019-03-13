ed7549b75780b72cc8ab554f069bdb9644ba125d
KHAUNA <jtwinturbo@gmail.com>
2019-03-12 21:37:45 -0400

    pull

---
+++ public/js/profile.js
@@ -0,0 +1,29 @@
+
+$(".submit").on('click', function(event) {
+    event.preventDefault();
+    console.log('connected');
+    var fullName = $('.fullName').val();
+    var interests = $('.interests').val();
+    var languages = $('.languages').val();
+    var aboutMe = $('.aboutMe').val();
+
+    var me = {
+        name: fullName,
+        interests: interests[0],
+        languages: languages[0],
+        aboutMe: aboutMe,
+    }
+
+    function newRider(){
+        $.ajax({
+            url: "/api/riders",
+            method: "POST",
+            data: me
+        }).then(function(err, data){
+            if (err) throw (err);
+            console.log('ajax')
+            console.log(data)
+        })
+    }
+    newRider();
+});
--- server.js
+++ server.js
@@ -9,6 +9,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static('login'));
+app.use(express.static('public'));
+
 
 require('./routes/apiRoutes')(app);
 require('./routes/htmlRoutes')(app);
