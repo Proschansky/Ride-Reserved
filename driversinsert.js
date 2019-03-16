259821c37ac75f4cdbc2a29855c27397ddee098d
Brzeczyszczykiewicz <Samproschansky@gmail.com>
2019-03-16 14:02:26 -0400

    Fixed the seed data inserts.

--- driversinsert.js
+++ driversinsert.js
@@ -1,100 +1,81 @@
-const dummyDrivers = require('./dummyDrivers').getDrivers();
-// const DataTypes = require('sequelize/lib/data-types');
-var Sequelize = require("sequelize");
-const db = require('./models');
-const Drivers = db.Drivers;
-// const driverpic = require('./public/img/driverpic');
-// var env = process.env.NODE_ENV || "development";
-require('dotenv').config();
+(async function () {
+    const dummyDrivers = require('./dummyDrivers').getDrivers();
+    // const DataTypes = require('sequelize/lib/data-types');
+    var Sequelize = require('sequelize');
+    const db = require('./models');
+    const Drivers = db.Drivers;
+    // const driverpic = require('./public/img/driverpic');
+    // var env = process.env.NODE_ENV || "development";
+    require('dotenv').config();
 
-var locations = ["NYC", "LA", "DC", "CHI", "ATL", "DNV", "BOST", "NSH",
-    "SEA", "SF", "ORL", "DAL", "HOU", "MIA", "STL", "NO"];
+    var locations = [
+        'NYC',
+        'LA',
+        'DC',
+        'CHI',
+        'ATL',
+        'DNV',
+        'BOST',
+        'NSH',
+        'SEA',
+        'SF',
+        'ORL',
+        'DAL',
+        'HOU',
+        'MIA',
+        'STL',
+        'NO',
+    ];
 
-console.log(locations.length);
+    const newLocation = index => {
+        const randomLocation = locations[Math.floor(Math.random() * (locations.length + 1))];
 
-function addALocation(){
-    var randomLocation = locations[Math.floor(Math.random() * (locations.length + 1))];
-    return randomLocation
-}
+        InsertLocation(randomLocation, index);
+    };
 
-const newLocation = async (index) => {
-    const randomLocation = await addALocation()
-    try {
-        InsertLocation(randomLocation, index)
-    } catch (error) {
-        console.error(error)
+    function InsertLocation(location, index) {
+        dummyDrivers[index].currentLocation = location;
     }
-}
 
-function InsertLocation(location, index) {
-    Drivers.update({
-        currentLocation: location ,
-    }, {
-            where: {
-                id: index
-            }
-        });
-}
+    // console.log(randomLocation);
+    // if (config.use_env_variable) {
+    //   var sequelize = new Sequelize(process.env[config.use_env_variable]);
+    // } else {
+    var sequelize = new Sequelize(process.env.database, process.env.name, process.env.password, {
+        host: 'localhost',
+        dialect: 'mysql',
+    });
 
-// console.log(randomLocation);
-// if (config.use_env_variable) {
-//   var sequelize = new Sequelize(process.env[config.use_env_variable]);
-// } else {
-var sequelize = new Sequelize(process.env.database, process.env.name, process.env.password, { host: 'localhost', dialect: 'mysql' });
+    const axios = require('axios');
 
-const axios = require('axios')
+    const url = 'https://randomuser.me/api/?results=1000';
+    const pics = await axios.get(url);
 
-const url = "https://randomuser.me/api/";
-const AxiosGet = async () => {
-    try {
-        return await axios.get(url)
-    } catch (error) {
-        console.error(error)
+    const DriverPic = index => {
+        InsertPic(pics.data.results[index].picture.medium, index);
+    };
+
+    function InsertPic(pictureUrl, index) {
+        dummyDrivers[index].picture = pictureUrl;
     }
-}
-const DriverPic = async (index) => {
-    const pics = await AxiosGet()
-    try {
-        InsertPic(pics.data.results[0].picture.medium, index)
-    } catch (error) {
-        console.error(error)
-    }
-}
 
-function InsertPic(pictureUrl, index) {
-    Drivers.update({
-        picture: pictureUrl,
-    }, {
-            where: {
-                id: index
-            }
-        });
-}
-
-function insertDrivers() {
-    // dummyDrivers.forEach(function(driver) {
-    // console.log(driver.interests)
-    db.sequelize.sync({ force: true }).then(function () {
+    function insertDrivers() {
         for (let i = 0; i < dummyDrivers.length; i++) {
-            Drivers.findOrCreate({
-                where: { name: dummyDrivers[i].name }, defaults: {
-                    interests: dummyDrivers[i].interests,
-                    aboutMe: dummyDrivers[i].aboutMe,
-                    languages: dummyDrivers[i].languages,
-                    availableStartDate: dummyDrivers[i].availableStartDate,
-                    availableEndDate: dummyDrivers[i].availableEndDate,
-                    currentLocation: "ATL"
-                }
-            }).catch(function (err) {
-                console.log('Error: ', err)
-            })
-           
             DriverPic(i);
             newLocation(i);
         }
+        db.sequelize.sync({ force: true }).then(function() {
+            Drivers.bulkCreate(dummyDrivers)
+                .then(function(dbDrivers) {
+                    console.log('Drivers inserted: ' + dbDrivers.length);
+                    db.sequelize.close();
+                })
+                .catch(function(err) {
+                    console.log('Error: ', err);
+                });
+        });
     }
-    )
-    
-}
 
-insertDrivers()
+    insertDrivers();
+
+})()
